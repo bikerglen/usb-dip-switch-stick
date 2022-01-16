02e341a8a0eab58c329f414e5d2596159abacf06
Glen Akins <ktmglen@glenakins.com>
2022-01-16 08:36:55 -0700

    initial checkin

---
+++ pic-software/usb-dip-switch.X/windows-software/USB-DIP-SWITCH/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace USB_DIP_SWITCH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 150);
+            this.button1.TabIndex = 0;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(106, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 150);
+            this.button2.TabIndex = 2;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(200, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 150);
+            this.button3.TabIndex = 3;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(294, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 150);
+            this.button4.TabIndex = 4;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(388, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 150);
+            this.button5.TabIndex = 5;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(482, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(88, 150);
+            this.button6.TabIndex = 6;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(576, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(88, 150);
+            this.button7.TabIndex = 7;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(670, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 150);
+            this.button8.TabIndex = 8;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Switch by suib icon from NounProject.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 204);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ pic-software/usb-dip-switch.X/windows-software/USB-DIP-SWITCH/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HidLibrary;
+
+namespace USB_DIP_SWITCH
+{
+    public partial class Form1 : Form
+    {
+        private static HidDevice _device;
+        public delegate void ReadHandlerDelegate(HidReport report);
+
+        Button[] buttons = new Button[8];
+        bool[] buttonStates = new bool[8];
+        Image[] buttonOff = new Image[8];
+        Image[] buttonOn = new Image[8];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonOff[0] = USB_DIP_SWITCH.Properties.Resources.brown_off;
+            buttonOn[0] = USB_DIP_SWITCH.Properties.Resources.brown_on;
+            buttonOff[1] = USB_DIP_SWITCH.Properties.Resources.red_off;
+            buttonOn[1] = USB_DIP_SWITCH.Properties.Resources.red_on;
+            buttonOff[2] = USB_DIP_SWITCH.Properties.Resources.orange_off;
+            buttonOn[2] = USB_DIP_SWITCH.Properties.Resources.orange_on;
+            buttonOff[3] = USB_DIP_SWITCH.Properties.Resources.yellow_off;
+            buttonOn[3] = USB_DIP_SWITCH.Properties.Resources.yellow_on;
+            buttonOff[4] = USB_DIP_SWITCH.Properties.Resources.green_off;
+            buttonOn[4] = USB_DIP_SWITCH.Properties.Resources.green_on;
+            buttonOff[5] = USB_DIP_SWITCH.Properties.Resources.blue_off;
+            buttonOn[5] = USB_DIP_SWITCH.Properties.Resources.blue_on;
+            buttonOff[6] = USB_DIP_SWITCH.Properties.Resources.violet_off;
+            buttonOn[6] = USB_DIP_SWITCH.Properties.Resources.violet_on;
+            buttonOff[7] = USB_DIP_SWITCH.Properties.Resources.gray_off;
+            buttonOn[7] = USB_DIP_SWITCH.Properties.Resources.gray_on;
+
+            buttons[0] = button1;
+            buttons[1] = button2;
+            buttons[2] = button3;
+            buttons[3] = button4;
+            buttons[4] = button5;
+            buttons[5] = button6;
+            buttons[6] = button7;
+            buttons[7] = button8;
+
+            for (int i = 0; i < 8; i++)
+            {
+                buttons[i].BackgroundImage = buttonOff[i];
+            }
+
+            _device = HidDevices.Enumerate(0x4247, 0x0019).FirstOrDefault();
+
+            if (_device == null)
+            {
+                MessageBox.Show("Unable to find the USB device.", "Device Not Found", MessageBoxButtons.OK);
+            }
+
+            _device.OpenDevice();
+            _device.MonitorDeviceEvents = true;
+            _device.ReadReport(ReadProcess);
+
+            // send an output report requesting the state of the switches
+            var outData = _device.CreateReport();
+            outData.ReportId = 0x02;
+            outData.Data[0] = 0x55;
+            _device.WriteReport(outData);
+        }
+
+        private void ReadProcess(HidReport report)
+        {
+            BeginInvoke(new ReadHandlerDelegate(ReadHandler), new object[] { report });
+        }
+
+        private void ReadHandler(HidReport report)
+        {
+            if (report.ReportId == 0x01)
+            {
+                int switches = report.Data[0];
+                for (int i = 0; i < 8; i++)
+                {
+                    int mask = 0x80 >> i;
+                    buttonStates[i] = ((switches & mask) == mask) ? true : false;
+                    UpdateButton(i);
+                }
+            }
+            _device.ReadReport(ReadProcess);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            for (int i = 0; i < 8; i++)
+            {
+                if (button == buttons[i])
+                {
+                    ToggleButtonState(i);
+                }
+            }
+        }
+
+        private void ToggleButtonState(int i)
+        {
+            buttonStates[i] = !buttonStates[i];
+            UpdateButton(i);
+        }
+
+        private void UpdateButton(int i)
+        {
+            buttons[i].BackgroundImage = buttonStates[i] ? buttonOn[i] : buttonOff[i];
+        }
+    }
+}
